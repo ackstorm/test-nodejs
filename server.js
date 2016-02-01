337ff3a30cfef5ed763ac4fba43cbac45f7d497a
Juan Carlos Moreno <juancarlos.moreno@ecmanaged.com>
2016-02-01 09:32:37 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+// server.js
+// load the things we need
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+var version = process.env.HOME;
+var uptime = process.env.LANGUAGE;
+
+// use res.render to load up an ejs view file
+
+// index page 
+app.get('/', function(req, res) {
+    res.render('pages/index', {
+        version: version, 
+	uptime: uptime
+    });
+});
+
+// about page 
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+app.listen(8080);
+console.log('8080 is the magic port');
